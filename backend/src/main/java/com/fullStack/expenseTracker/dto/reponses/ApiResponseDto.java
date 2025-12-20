ea73298601b45f33f4620dc2ff920ba7f6f39c4d
GarvitKansal <garvitkansal21@gmail.com>
2025-12-20 21:45:39 +0530

    Initial commit

---
+++ backend/src/main/java/com/fullStack/expenseTracker/dto/reponses/ApiResponseDto.java
@@ -0,0 +1,18 @@
+package com.fullStack.expenseTracker.dto.reponses;
+
+import com.fullStack.expenseTracker.enums.ApiResponseStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+public class ApiResponseDto<T> {
+    private ApiResponseStatus status;
+
+    private HttpStatus httpStatus;
+
+    private T response;
+
+
+}
